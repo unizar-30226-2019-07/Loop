ff74cc793e99abae3ade07df04f7ce07e883a7ec
unknown <diego.roymen@gmail.com>
2019-03-16 11:30:04 +0100

    Decoracion en la parte superior del perfil

--- lib/screens/users/profile.dart
+++ lib/screens/users/profile.dart
@@ -1,154 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:selit/models/usuario_model.dart';
+import 'package:selit/util/star_rating.dart';
 
+/// Perfil de usuario, tal como aparece en el wireframe
+/// dividido en dos partes: profile_user (superior) y profile_products (inferior)
 class Profile extends StatefulWidget {
-	@override
-	ProfileState createState() => new ProfileState();
+  @override
+  _ProfileState createState() => new _ProfileState();
 }
 
-// TODO mover a otro fichero
-class UsuarioVO {
+/* TODO ver https://marcinszalek.pl/flutter/filter-menu-ui-challenge/
+ * para el diseño del perfil
+class _ProfileClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+    path.lineTo(0.0, size.height - 60.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
 
-  // Atributos a mostrar del usuario en el perfil
-  Text nombre;
-  Text apellidos;
-  dynamic fotoPerfil; // TODO tipo común image-fadeinimage
-  Text sexoEdad;
-  Text ubicacion;
-  double numeroEstrellas;
-  Text reviews;
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
+*/
 
+class _ProfileState extends State<Profile> {
   // Estilos para los diferentes textos
-	static final _styleNombre = const TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0);
-	static final _styleSexoEdad = const TextStyle(fontStyle: FontStyle.italic, fontSize: 15.0);
-	static final _styleUbicacion = const TextStyle(fontSize: 15.0);
-	static final _styleReviews = const TextStyle(fontSize: 15.0);
+  static final _styleNombre = const TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.white);
+  static final _styleSexoEdad = const TextStyle(
+      fontStyle: FontStyle.italic, fontSize: 15.0, color: Colors.white);
+  static final _styleUbicacion =
+      const TextStyle(fontSize: 15.0, color: Colors.white);
+  static final _styleReviews =
+      const TextStyle(fontSize: 15.0, color: Colors.black);
+  static final _textAlignment = TextAlign.left;
 
-  // TODO cargar datos del usuario del JSON
-  UsuarioVO(jsonData) {
-    nombre = new Text('Nombre', style: _styleNombre, textAlign: TextAlign.left);
-    apellidos = new Text('Apellidos', style: _styleNombre, textAlign: TextAlign.left);
-    sexoEdad = new Text('Hombre, 21 años', style: _styleSexoEdad, textAlign: TextAlign.left);
-    ubicacion = new Text('Zaragoza, España', style: _styleUbicacion, textAlign: TextAlign.left);
-    fotoPerfil = FadeInImage.assetNetwork(
-      placeholder: 'images/profile_default.jpg',
-      image: 'https://avatars0.githubusercontent.com/u/17049331',
-      fadeInCurve: Curves.linear,
-      fadeInDuration: const Duration(milliseconds: 100),
-    );
-    numeroEstrellas = 4.5;
-    reviews = new Text('30 reviews', style:_styleReviews);
+  /// Usuario a mostrar en el perfil
+  UsuarioModel _user = UsuarioModel.placeholder();
+
+  /// Realiza una petición GET para obtener los datos del usuario
+  /// userId y al recibirlos actualiza el perfil para que muestre
+  /// los datos de dicho usuario
+  Future<void> _loadProfile(userId) async {
+    // TODO hacer una petición en lugar de simular una carga de 1 segundo
+    return Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _user = new UsuarioModel(
+            nombre: 'Nombre',
+            apellidos: 'Apellidos',
+            sexo: 'Hombre',
+            edad: 21,
+            ubicacionCiudad: 'Zaragoza',
+            ubicacionResto: 'Aragon, España',
+            numeroEstrellas: 2.5,
+            reviews: 30,
+            urlPerfil:
+                'https://avatars0.githubusercontent.com/u/17049331'); // TODO modificar por JSON
+      });
+    });
   }
 
-  // Usuario por defecto, para mostrarlo mientras carga
-  UsuarioVO.placeholder() {
-    nombre = new Text('-----', style: _styleNombre, textAlign: TextAlign.left);
-    apellidos = new Text('-----', style: _styleNombre, textAlign: TextAlign.left);
-    sexoEdad = new Text('---, ---', style: _styleSexoEdad, textAlign: TextAlign.left);
-    ubicacion = new Text('---', style: _styleUbicacion, textAlign: TextAlign.left);
-    fotoPerfil = Image.asset(
-      'images/profile_default.jpg',
-    );
-    numeroEstrellas = 0;
-    reviews = new Text('---', style:_styleReviews);
-  }
-
-}
-
-class ProfileState extends State<Profile> {
-
-  UsuarioVO user = UsuarioVO.placeholder();
-
-  // Perfil por defecto, sin ningún dato
-  // Se muestra hasta que se reciban los datos de la petición HTTP
+  /// Widget correspondiente al perfil del usuario _user
   Widget _buildProfile() {
-    Widget widgetStar = Expanded(
-      flex: 2,
-      child: Icon(Icons.star),
-    );
+    // wUserData (parte superior)
+    // - wUserDataLeft (parte izquierda: foto de perfil, estrellas)
+    // - wUserDataRight (parte derecha: nombre, apellidos, etc.)
 
-    // TODO mover estrellas a carpeta util
-    Widget widgetUserStars = Column(
-      children: <Widget>[
-        Row(
+    Widget wUserDataLeft = Expanded(
+      flex: 4,
+      child: Container(
+        margin: EdgeInsets.only(left: 25),
+        child: Column(
           children: <Widget>[
-            widgetStar,
-            widgetStar,
-            widgetStar,
-            widgetStar,
-            widgetStar,
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: ClipOval(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: _user.fotoPerfil,
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 10),
+                child: StarRating(starRating: _user.numeroEstrellas ?? 5)),
+            Container(
+                margin: EdgeInsets.only(top: 5, bottom: 15),
+                alignment: Alignment.center,
+                child: Text('${_user.reviews} reviews',
+                    style: _styleReviews, textAlign: _textAlignment))
           ],
-        )
-      ],
+        ),
+      ),
     );
 
-    // TODO separar en mas variables?
-    Widget widgetUserData = Row(
-      children: <Widget>[
-        // Foto de perfil y estrellas
-        Expanded(
-          flex: 4,
-          child: Container(
-            margin: EdgeInsets.only(left: 25),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child: user.fotoPerfil,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: widgetUserStars
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  alignment: Alignment.center,
-                  child: user.reviews,
-                )
-              ],
-            ),
-          ),
-        ),
-        // Nombre, sexo, edad, ubicación
-        Expanded(
-          flex: 6,
-          child: Container(
-            margin: EdgeInsets.only(left: 25, right: 10),
-            //color: Colors.red, // util para ajustar margenes
-            child: Column(
-              children: <Widget>[
-                Container(
+    Widget wUserDataRight = Expanded(
+      flex: 6,
+      child: Container(
+          margin: EdgeInsets.only(left: 25, right: 10),
+          //color: Colors.green, // util para ajustar margenes
+          child: Column(
+            children: <Widget>[
+              Container(
                   alignment: Alignment.topLeft,
-                  margin: EdgeInsets.only(top: 30),
-                  child: user.nombre,
-                ),
-                Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text(_user.nombre ?? '---',
+                      style: _styleNombre, textAlign: _textAlignment)),
+              Container(
                   alignment: Alignment.topLeft,
                   margin: EdgeInsets.only(top: 5),
-                  child: user.apellidos,
-                ),
-                Container(
+                  child: Text(_user.apellidos ?? '---',
+                      style: _styleNombre, textAlign: _textAlignment)),
+              Container(
                   alignment: Alignment.topLeft,
                   margin: EdgeInsets.only(top: 10),
-                  child: user.sexoEdad
+                  child: Text('${_user.sexo}, ${_user.edad} años',
+                      style: _styleSexoEdad, textAlign: _textAlignment)),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 15),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.all(2),
+                      child: Icon(Icons.location_on),
+                    ),
+                    Text(
+                      _user.ubicacionCiudad ?? '---',
+                      style: _styleUbicacion,
+                      textAlign: _textAlignment,
+                    ),
+                  ],
                 ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.only(top: 15, bottom: 50),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:EdgeInsets.all(5),
-                        child: Icon(Icons.location_on),
-                      ),
-                      user.ubicacion,
-                    ],
-                  ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 5, bottom: 45),
+                child: Text(
+                  _user.ubicacionResto ?? '---',
+                  style: _styleUbicacion,
+                  textAlign: _textAlignment,
                 ),
-              ],
-            )
-          ),
-        )
-      ],
+              )
+            ],
+          )),
+    );
+
+    Widget wUserData = Container(
+      // TODO seguro que hay alguna forma mejor de añadir un color de fondo
+      // que usar un gradiente de este modo, buscar informacion
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            stops: [
+              0.65,
+              0.65
+            ],
+            colors: [
+              Theme.of(context).primaryColor,
+              Colors.grey[100],
+            ]),
+      ),
+      padding: EdgeInsets.only(top: 30),
+      child: Row(children: <Widget>[wUserDataLeft, wUserDataRight]),
     );
 
     // TODO completar
@@ -156,10 +177,7 @@ class ProfileState extends State<Profile> {
 
     return Column(
       children: <Widget>[
-        // Parte superior: datos del usuario
-        widgetUserData,
-        Divider(),
-        // Parte inferior: objetos en venta y vendidos
+        wUserData,
         Expanded(
           child: widgetUserProducts,
         )
@@ -167,30 +185,9 @@ class ProfileState extends State<Profile> {
     );
   }
 
-  // Muestra el perfil por defecto e inicia una petición
-  // para obtener los datos del usuario userId
-  // Cuando recibe los datos, actualiza el perfil
-  Future<void> _loadProfile(userId) async {
-    // TODO hacer una petición en lugar de simular una carga de 1 segundo
-    return Future.delayed(
-			Duration(seconds: 1),
-      () {
-        setState(() {
-          user = new UsuarioVO('testData'); // TODO modificar por JSON
-        });
-      }
-    );
+  @override
+  Widget build(BuildContext context) {
+    _loadProfile(1); // TODO sustituir "1" por el ID pasado
+    return Scaffold(body: _buildProfile());
   }
-
-	@override
-	Widget build(BuildContext context) {
-    _loadProfile(1);
-		return Scaffold(
-			appBar: AppBar(
-				title: const Text('Perfil'),
-			),
-			body: _buildProfile(),
-		);
-	}
-
 }
