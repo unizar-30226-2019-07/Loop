1e41706fadb253ec3b091f3c6ead5b13f9d8c041
vpec <vpe1@protonmail.com>
2019-03-22 19:20:16 +0100

    Pequeño arreglo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,3 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:selit/ui/login_page.dart';
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Selit!',
-      theme: new ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: new LoginPage(),
-    );
-  }
-}
-*/
-
 import 'routes.dart';
 
 void main() => new Routes();
