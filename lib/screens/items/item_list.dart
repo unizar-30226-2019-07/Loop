6a42692462cedfe8651632ee428a57aa4d29e0e9
diegoroyo <diego.roymen@gmail.com>
2019-03-26 18:59:14 +0100

    Actualizado item_list a ultima version

--- lib/screens/items/item_list.dart
+++ lib/screens/items/item_list.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:selit/class/item_class.dart';
-import 'package:selit/util/api/item_request.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:selit/widgets/items/item_tile.dart';
+import 'package:selit/util/api/item_request.dart';
+import 'package:selit/class/item_class.dart';
 import 'dart:async';
 
-/// Lista con varios productos (por ahora muestra fotos de cervezas
-/// por motivos de test) en vista de 1 columna
+
 class ItemList extends StatefulWidget {
   
+
   @override
   _ItemList createState() => _ItemList();
 
@@ -16,6 +17,9 @@ class ItemList extends StatefulWidget {
 class _ItemList extends State<ItemList> {
 
   List<ItemClass> _items = <ItemClass>[];
+
+  List<String> _tags=['hola'];
+  IconData _times = FontAwesomeIcons.times;
 
   static const IconData tune = IconData(0xe429, fontFamily: 'MaterialIcons');
 
@@ -34,10 +38,97 @@ class _ItemList extends State<ItemList> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+
+
+    /*
+    appBar: AppBar(
+      centerTitle: true,
+      title: Text('Productos'),
+    ),*/
     body: SafeArea(
       child: Container(
         child: Column(
-          children: <Widget>[ 
+          children: <Widget>[
+            /*Row(
+              children: <Widget>[
+                
+              ],
+            ),*/
+            Padding(
+              padding: const EdgeInsets.only(bottom:8.0, top:8.0, right:8.0, left:8.0),
+                child: TextField(
+                  onSubmitted: (value) {
+                    print(value);
+                  },
+                  /*onChanged: (value) {
+                    print(value);
+                  },*/
+                  //controller: editingController,
+                  decoration: InputDecoration(
+                    labelText: "Buscar",
+                    hintText: "Buscar",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))
+                    ),
+                    icon: GestureDetector(
+                              onTap: (){
+                                print('filter_list was tapped');
+                              },
+                              child: Icon(
+                                tune,
+                                size: 30.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                  ),
+                  
+                ),
+              ),   
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5.0),
+              height: 50.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  /*
+                  ListTile(
+                    leading: Icon(_times),
+                    title: Text('etiqueta')
+                  ),
+
+                  ListTile(
+                    leading: Icon(_times),
+                    title: Text('etiqueta 2')
+                  ),
+                  */
+                  Container(
+                    width: 160.0,
+                    child: ListTile(
+                      leading: Icon(_times),
+                      title: Text('etiqueta 1 se sale por abajo'),
+                    ),
+                    
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               child: ListView.builder(
                 itemCount: _items.length,
