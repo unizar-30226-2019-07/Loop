9d2bf568b30f8708f323097bb3c4ffdc91c21ec8
diegoroyo <diego.roymen@gmail.com>
2019-03-26 17:20:44 +0100

    Refactor carpeta images -> assets

--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -12,13 +12,13 @@ class ProfilePicture extends StatelessWidget {
     if (_url == null || _url.isEmpty) {
       // No existe foto de perfil: foto por defecto
       return Image.asset(
-        'images/profile_default.jpg',
+        'assets/img/profile_default.jpg',
       );
     } else {
       // Tiene foto de perfil: mostrar la foto por defecto
       // hasta que cargue la foto real
       return FadeInImage.assetNetwork(
-        placeholder: 'images/profile_default.jpg',
+        placeholder: 'assets/img/profile_default.jpg',
         image: _url,
         fadeInCurve: Curves.linear,
         fadeInDuration: const Duration(milliseconds: 100),
