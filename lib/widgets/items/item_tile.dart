a9a845bb80dfd5d6b151f4bf5dfebafa558ea335
unknown <diego.roymen@gmail.com>
2019-04-07 18:40:11 +0200

    Nuevos estilos para ItemTile e ItemTileVertical

--- lib/widgets/items/item_tile.dart
+++ lib/widgets/items/item_tile.dart
@@ -7,6 +7,14 @@ class ItemTile extends StatelessWidget {
   final ItemClass _item;
   ItemTile(this._item);
 
+  static final _styleTitle =
+      TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  static final _styleDescription =
+      TextStyle(fontSize: 14.0, color: Colors.grey[700]);
+  // Nota: stylePrice usa el color rojo de la aplicación (ver más abajo)
+  static final _stylePrice =
+      TextStyle(fontSize: 16.0, fontWeight: FontWeight.w900);
+
   static const double height = 129.0;
 
   @override
@@ -24,28 +32,97 @@ class ItemTile extends StatelessWidget {
               child: Card(
                 // This ensures that the Card's children (including the ink splash) are clipped correctly.
                 clipBehavior: Clip.antiAlias,
-                //shape: shape,
+                elevation: 2.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Colors.grey[300], width: 1.0)),
                 child: InkWell(
-                  onTap: () => Navigator.of(context).pushNamed('/item-details', arguments: _item),
-                  // Generally, material cards use onSurface with 12% opacity for the pressed state.
-                  splashColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
-                  // Generally, material cards do not have a highlight overlay.
+                  onTap: () => Navigator.of(context)
+                      .pushNamed('/item-details', arguments: _item),
+                  splashColor:
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.05),
                   highlightColor: Colors.transparent,
-                  child:
-                  Container(
-                    margin: EdgeInsets.only(bottom: 6.0),
-                    child: ListTile(
-                      title: Text(_item?.title ?? '---'),
-                      trailing: Text('${_item?.price} ${_item?.currency}'),
-                      subtitle: Text(_item.description ?? '---', overflow: TextOverflow.ellipsis, textAlign: TextAlign.justify,maxLines:5),
-                      leading: Container(
-                        margin: EdgeInsets.only(left: 6.0, bottom: 15.0),
-                        child: Image.network('https://i.imgur.com/rqSvE0T.png', // TODO sustituir por .images
-                          width: 65.0, fit: BoxFit.contain,)
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      // TODO si el producto no tiene imágenes, no mostrar ni Image ni SizedBox
+                      // Primera imágen del producto
+                      Image.network(
+                          'https://images.pexels.com/photos/1140991/pexels-photo-1140991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', // TODO sustituir por .images
+                          width: 100.0,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                          color: Colors.white12, // filtro para blanquear
+                          colorBlendMode: BlendMode.srcOver),
+                      // Borde entre la imagen y el resto
+                      SizedBox.fromSize(
+                        size: Size(1.0, double.infinity),
+                        child: Container(color: Colors.grey[300]),
                       ),
-                    ),
+                      // Titulo, descripción, precio
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 7.0, horizontal: 10.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(_item?.title ?? '---',
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  style: _styleTitle),
+                              Expanded(
+                                child: Stack(
+                                  children: <Widget>[
+                                    ClipRect(
+                                      child: Text(_item?.description ?? '---',
+                                          maxLines: 10,
+                                          style: _styleDescription),
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                            begin: Alignment(0, -1.0),
+                                            end: Alignment(0, 1.0),
+                                            stops: [
+                                              0.8,
+                                              0.9
+                                            ],
+                                            colors: [
+                                              Colors.white12,
+                                              Colors.white,
+                                            ]),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox.fromSize(
+                                size: Size(double.infinity, 20.0),
+                                child: Text(
+                                  '${_item?.price} ${_item?.currency}',
+                                  textAlign: TextAlign.end,
+                                  style: _stylePrice.copyWith(
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  
+                  /*ListTile(
+                        title: Text(_item?.title ?? '---'),
+                        subtitle: Text(_item.description ?? '---', overflow: TextOverflow.ellipsis, textAlign: TextAlign.left,maxLines:5),
+                        trailing: Text('${_item?.price} ${_item?.currency}'),
+                        leading: Container(
+                          margin: EdgeInsets.only(left: 6.0, bottom: 15.0),
+                          child: Image.network('https://i.imgur.com/rqSvE0T.png', // TODO sustituir por .images
+                            width: 65.0, fit: BoxFit.fill)
+                        ),
+                      ),*/
                 ),
               ),
             ),
--- lib/widgets/items/item_tile_vertical.dart
+++ lib/widgets/items/item_tile_vertical.dart
@@ -6,6 +6,14 @@ class ItemTileVertical extends StatelessWidget {
   final ItemClass _item;
   ItemTileVertical(this._item);
 
+  static final _styleTitle =
+      TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  static final _styleDescription =
+      TextStyle(fontSize: 14.0, color: Colors.grey[700]);
+  // Nota: stylePrice usa el color rojo de la aplicación (ver más abajo)
+  static final _stylePrice =
+      TextStyle(fontSize: 16.0, fontWeight: FontWeight.w900);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -21,93 +29,71 @@ class ItemTileVertical extends StatelessWidget {
               child: Card(
                 // This ensures that the Card's children (including the ink splash) are clipped correctly.
                 clipBehavior: Clip.antiAlias,
-                //shape: shape,
+                elevation: 2.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Colors.grey[300], width: 1.0)),
                 child: InkWell(
                   onTap: () => Navigator.of(context).pushNamed('/item-details', arguments: _item),
-                  // Generally, material cards use onSurface with 12% opacity for the pressed state.
                   splashColor:
-                      Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
-                  // Generally, material cards do not have a highlight overlay.
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.05),
                   highlightColor: Colors.transparent,
-                  child: Container(
-                      margin: EdgeInsets.only(bottom: 6.0),
-                      child: Column(children: <Widget>[
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 10,
-                                ),
-                                child: Container(
-                                  constraints: new BoxConstraints(
-                                      maxWidth:
-                                          MediaQuery.of(context).size.width -
-                                              255),
-                                  child: Image.network(
-                                    'https://i.imgur.com/rqSvE0T.png', // TODO sustituir por .images
-                                    width: 65.0,
-                                    fit: BoxFit.contain,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      // Imagen superior: máximo de 200 píxeles
+                      SizedBox(
+                        width: double.infinity,
+                        child: Container(
+                          constraints: BoxConstraints(maxHeight: 200.0),
+                          child: Image.network(
+                            'https://images.pexels.com/photos/845405/pexels-photo-845405.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', // TODO sustituir por .images
+                            fit: BoxFit.cover,
+                            color: Colors.white12, // filtro para blanquear
+                            colorBlendMode: BlendMode.srcOver,
+                          ),
+                        ),
+                      ),
+                      // Borde entre la imagen y el resto
+                      SizedBox.fromSize(
+                        size: Size(double.infinity, 1.0),
+                        child: Container(color: Colors.grey[300]),
+                      ),
+                      // Título, descripción y precio
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 7.0, horizontal: 10.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(_item?.title ?? '---',
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  style: _styleTitle),
+                              Text(
+                                _item?.description ?? '---',
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 6,
+                                style: _styleDescription
+                              ),
+                              SizedBox.fromSize(
+                                size: Size(double.infinity, 30.0),
+                                child: Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Text('${_item?.price} ${_item?.currency}',
+                                    textAlign: TextAlign.end,
+                                    style: _stylePrice.copyWith(
+                                        color:
+                                            Theme.of(context).primaryColor),
                                   ),
                                 ),
                               ),
-                            ]),
-                        Row(children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                              top: 20,
-                            ),
-                            child: Container(
-                              constraints: new BoxConstraints(
-                                  maxWidth:
-                                      MediaQuery.of(context).size.width - 255),
-                              child: Text(_item?.title ?? '---',
-                              style: new TextStyle(
-                                fontSize: 16.0,
-                                ),),
-                            ),
-                          )
-                        ]),
-                        Row(children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                              top: 5,
-                            ),
-                            child: Container(
-                              constraints: new BoxConstraints(
-                                  maxWidth:
-                                      MediaQuery.of(context).size.width - 260),
-                              child: Text(
-                                _item?.description ?? '---',
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.justify,
-                                maxLines: 7,
-                                style: new TextStyle(
-                                  color: Colors.grey[600],
-                                ),
-                              ),
-                            ),
-                          )
-                        ]),
-                        Row(children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                              top: 20,
-                            ),
-                            child: Container(
-                              child: Text('${_item?.price} ${_item?.currency}',
-                              style: new TextStyle(
-                                
-                                fontSize: 16.0,
-                                ),),
-                            ),
-                          )
-                        ]),
-                      ])),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
                       
                 ),
               ),
