585a8e2d7d70cbb0cbccde21233cbcd4e9df4978
javiermixture17 <javiermf.98@gmail.com>
2019-04-14 18:43:17 +0200

    Cambio de contraseña

--- lib/screens/users/edit_profile.dart
+++ lib/screens/users/edit_profile.dart
@@ -6,7 +6,9 @@ import 'package:selit/widgets/profile_picture.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:selit/util/api/usuario_edit.dart';
 import 'package:mime/mime.dart';
+import 'package:selit/util/storage.dart';
 import 'package:path/path.dart' as path;
+import 'package:selit/util/api/usuario_request.dart';
 
 /// Página de edición de perfil (formulario con los campos
 /// necesarios para modificar los atributos del usuario)
@@ -30,14 +32,18 @@ class _EditProfileState extends State<EditProfile> {
   final TextEditingController _locationController = new TextEditingController();
   final TextEditingController _sexController = new TextEditingController();
   final TextEditingController _yearController = new TextEditingController();
-    final TextEditingController _oldPassController = new TextEditingController();
-      final TextEditingController _newPassController = new TextEditingController();
-        final TextEditingController _newPassRepController = new TextEditingController();
+  final TextEditingController _oldPassController = new TextEditingController();
+  final TextEditingController _newPassController = new TextEditingController();
+  final TextEditingController _newPassRepController =
+      new TextEditingController();
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   static final _styleTitle = TextStyle(
       fontSize: 22.0, color: Colors.black, fontWeight: FontWeight.bold);
+
+  final Color _colorStatusBarGood = Colors.blue.withOpacity(0.5);
+  final Color _colorStatusBarBad = Colors.red.withOpacity(0.5);
 
   /// Usuario a mostrar en el perfil
   UsuarioClass _user;
@@ -46,7 +52,7 @@ class _EditProfileState extends State<EditProfile> {
   File _galleryFile;
   String _base64Image;
   String _mimeType;
-  String _charset; 
+  String _charset;
 
   //Lista opciones sexo
   List<String> _sexos = <String>['', 'hombre', 'mujer', 'Otro'];
@@ -84,7 +90,8 @@ class _EditProfileState extends State<EditProfile> {
     if (_nameController.text.length < 1 || _surnameController.text.length < 1) {
       showInSnackBar("Rellena toodos los campos correctamente", Colors.yellow);
     } else {
-      _user.update(_nameController.text, _surnameController.text, _sexo, 3, 3, _mimeType, _base64Image , _charset);
+      _user.update(_nameController.text, _surnameController.text, _sexo, 3, 3,
+          _mimeType, _base64Image, _charset);
       edit(_user).then((response) {
         final Color legit = Colors.blue.withOpacity(0.5);
         final Color fake = Colors.red.withOpacity(0.5);
@@ -110,20 +117,34 @@ class _EditProfileState extends State<EditProfile> {
     }
   }
 
-  void cambioPass() {
-
+  void cambioPass() async {
     if (_newPassController.text != _newPassRepController.text) {
       showInSnackBar("Las contraseñas no coinciden", Colors.yellow);
-    } 
-    else if(_newPassController.text.length <=0 || _newPassRepController.text.length <=0 
-      || _oldPassController.text.length <=0 ){
-        showInSnackBar("Completa todos los campos", Colors.yellow);
-    }
-    else{
+    } else if (_newPassController.text.length <= 0 ||
+        _newPassRepController.text.length <= 0 ||
+        _oldPassController.text.length <= 0) {
+      showInSnackBar("Completa todos los campos", Colors.yellow);
+    } else {
+      int miId = await Storage.loadUserId();      
+      UsuarioRequest.password(
+              _oldPassController.text, _newPassController.text, miId)
+          .then((_) {
+        showInSnackBar("Contraseña actualizada correctamente", _colorStatusBarGood);
+        Navigator.of(context).pop();
+        Navigator.of(context).pop();
+      }).catchError((error) {
+        if (error == "No autorizado" || error == "Prohibido" || error =="No encontrado") {
+          showInSnackBar("Acción no autorizada", _colorStatusBarBad);
+        } else if(error == "Precondición fallida (contraseña actual incorrecta)"){
+          showInSnackBar("Contraseña incorrecta", _colorStatusBarBad);
+        }
+        else {
+          showInSnackBar("No hay conexión a internet", _colorStatusBarBad);
+        }
+      });
 
     }
   }
-
 
   /// Widget correspondiente a la edición del perfil del usuario _user
   /// Si un campo de _user es nulo, se muestran los campos por defecto
@@ -246,7 +267,6 @@ class _EditProfileState extends State<EditProfile> {
               //color: Colors.red, // util para ajustar margenes
               child: Column(
                 children: <Widget>[
-                  
                   new TextFormField(
                     decoration: const InputDecoration(
                       labelText: 'Contraseña antigua',
@@ -254,7 +274,6 @@ class _EditProfileState extends State<EditProfile> {
                     controller: _oldPassController,
                     keyboardType: TextInputType.emailAddress,
                   ),
-                  
                 ],
               )),
         )
@@ -410,11 +429,7 @@ class _EditProfileState extends State<EditProfile> {
                       },
                     )),
                 Padding(
-                  padding: EdgeInsets.only(
-                    left: 6,
-                    top: 40,
-                    bottom: 10
-                  ),
+                  padding: EdgeInsets.only(left: 6, top: 40, bottom: 10),
                   child: Row(children: <Widget>[
                     Row(children: <Widget>[
                       Text('Cambiar contraseña', style: _styleTitle)
@@ -425,7 +440,8 @@ class _EditProfileState extends State<EditProfile> {
                 Divider(),
                 wPassword,
                 new Container(
-                    padding: const EdgeInsets.only(left: 10.0, top: 20.0, bottom: 55),
+                    padding: const EdgeInsets.only(
+                        left: 10.0, top: 20.0, bottom: 55),
                     child: new RaisedButton(
                       color: Color(0xffc0392b),
                       child: const Text('Cambiar contraseña',
--- lib/util/api/usuario_request.dart
+++ lib/util/api/usuario_request.dart
@@ -8,24 +8,24 @@ import 'dart:io';
 
 /// Interacciones con la API relacionadas con usuarios
 class UsuarioRequest {
-
   /// Login de usuario por [email] y [password], devuelve un token si ha ido bien
   static Future<TokenClass> login(String email, String password) async {
-    http.Response response =
-        await http.post('${APIConfig.BASE_URL}/login', headers: {
+    http.Response response = await http.post('${APIConfig.BASE_URL}/login',
+        headers: {
           HttpHeaders.contentTypeHeader: ContentType.json.toString(),
-    }, body: json.jsonEncode({
-      "email": email,
-      "password": password,
-    }));
-    print("Resultado del login: Codigo ${response.statusCode}, body ${response.headers[HttpHeaders.authorizationHeader]}");
+        },
+        body: json.jsonEncode({
+          "email": email,
+          "password": password,
+        }));
+    print(
+        "Resultado del login: Codigo ${response.statusCode}, body ${response.headers[HttpHeaders.authorizationHeader]}");
     switch (response.statusCode) {
       case 200: // Login OK
         TokenClass receivedToken =
             TokenClass(response.headers[HttpHeaders.authorizationHeader]);
         Storage.saveToken(receivedToken.token);
-        UsuarioClass receivedUser = 
-            await UsuarioRequest.getUserById(0);
+        UsuarioClass receivedUser = await UsuarioRequest.getUserById(0);
         Storage.saveUserId(receivedUser.user_id);
         return receivedToken;
         break;
@@ -40,10 +40,12 @@ class UsuarioRequest {
 
   /// Registro de usuario con ciertos campos de [UsuarioClass] con contraseña [password]
   static Future<bool> signUp(UsuarioClass newUser, String password) async {
-    http.Response response =
-        await http.post('${APIConfig.BASE_URL}/users', headers: {
+    http.Response response = await http.post('${APIConfig.BASE_URL}/users',
+        headers: {
           HttpHeaders.contentTypeHeader: ContentType.json.toString(),
-    }, body: json.jsonEncode(newUser.toJsonForSignUp()..addAll({"password": password})));
+        },
+        body: json.jsonEncode(
+            newUser.toJsonForSignUp()..addAll({"password": password})));
     switch (response.statusCode) {
       case 201: // Registro OK, recurso creado (201)
         return true;
@@ -55,25 +57,26 @@ class UsuarioRequest {
 
   /// Obtener los datos del usuario con ID [userId]
   static Future<UsuarioClass> getUserById(int userId) async {
-    print (userId);
+    print(userId);
     http.Response response;
     if (userId == 0) {
       print('GET /users/me');
       response = await http.get('${APIConfig.BASE_URL}/users/me', headers: {
-          HttpHeaders.contentTypeHeader: ContentType.json.toString(),
-          HttpHeaders.authorizationHeader: await Storage.loadToken(),
+        HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+        HttpHeaders.authorizationHeader: await Storage.loadToken(),
       });
     } else {
       print('GET /users/$userId');
-      response = await http.get('${APIConfig.BASE_URL}/users/$userId', headers: {
-          HttpHeaders.contentTypeHeader: ContentType.json.toString(),
-          HttpHeaders.authorizationHeader: await Storage.loadToken(),
+      response =
+          await http.get('${APIConfig.BASE_URL}/users/$userId', headers: {
+        HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+        HttpHeaders.authorizationHeader: await Storage.loadToken(),
       });
     }
     switch (response.statusCode) {
       case 200: // El usuario se ha devuelto bien
         return UsuarioClass.fromJson(json.jsonDecode(response.body));
-      default:  // TODO casos de error
+      default: // TODO casos de error
         return null;
     }
   }
@@ -96,4 +99,45 @@ class UsuarioRequest {
         throw ("Error al obtener la lista de usuarios, obtenido el código ${response?.statusCode}");
     }
   }
+
+  static Future<TokenClass> password(String oldPassword, String newPassword,int user_id ) async {
+
+    print(
+       json.jsonEncode({
+          "old": oldPassword,
+          "new": newPassword,
+        }));
+
+    print(
+        '${APIConfig.BASE_URL}/users/${user_id}/change_password');
+
+    http.Response response = await http.post('${APIConfig.BASE_URL}/users/${user_id}/change_password',
+         headers: {
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: await Storage.loadToken(),
+      },
+        body: json.jsonEncode({
+          "old": oldPassword,
+          "new": newPassword,
+        }));
+
+        print(
+        "Resultado del cambio de contraseña: Codigo ${response.statusCode}");
+            
+    switch (response.statusCode) {
+      case 200: // todo OK
+        break;
+      case 401:
+        throw("No autorizado");
+      case 402:
+        throw("Prohibido");
+      case 404:
+        throw("No encontrado");
+      case 412:
+        throw("Precondición fallida (contraseña actual incorrecta)");
+      default:
+        throw ("Unknown");
+    }
+  }
+
 }
