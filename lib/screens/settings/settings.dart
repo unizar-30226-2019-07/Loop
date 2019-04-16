f131abfea6baff4b73b42896b52ab0c91010c32d
javiermixture17 <javiermf.98@gmail.com>
2019-04-16 14:13:57 +0200

    Eliminar cuenta y cerrar sesión

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:selit/util/storage.dart';
 
 // Vista temporal con varios botones que llevan a diferentes vistas
@@ -6,37 +7,42 @@ import 'package:selit/util/storage.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Theme.of(context).primaryColor);
     return Scaffold(
         appBar: AppBar(
+          leading: Container(),
           title: Text('Ajustes'),
         ),
         body: Container(
             margin: EdgeInsets.all(20),
-            child: ListView(children: <Widget>[
-              ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text("Cuenta"),
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/account');
-                  }),
-              ListTile(leading: Icon(Icons.help), title: Text("Ayuda")),
-              ListTile(
-                  leading: Icon(Icons.notifications),
-                  title: Text("Notificaciones")),
-              Padding(
-                  padding: EdgeInsets.only(
-                    top: 410,
-                  ),
-                  child: RaisedButton(
-                    color: Color(0xffc0392b),
-                    child: const Text('Cerrar sesión',
-                        style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      Storage.deleteToken();
-                      print("Sesión cerrada");
-                      //Navigator.pushReplacementNamed(context, "/login-page");
-                    },
-                  ))
+            child: Column(children: [
+              Expanded(
+                child: ListView(children: <Widget>[
+                  ListTile(
+                      leading: Icon(Icons.account_circle),
+                      title: Text("Cuenta"),
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/account');
+                      }),
+                  ListTile(
+                      leading: Icon(Icons.notifications),
+                      title: Text("Notificaciones")),
+                  ListTile(leading: Icon(Icons.help), title: Text("Ayuda")),
+                ]),
+              ),
+              ButtonTheme(
+                minWidth: 150.0,
+                child: RaisedButton(
+                  color: Color(0xffc0392b),
+                  child: const Text('Cerrar sesión',
+                      style: TextStyle(color: Colors.white)),
+                  onPressed: () {
+                    Storage.deleteToken();
+                    print("Sesión cerrada");
+                    //Navigator.pushReplacementNamed(context, "/login-page");
+                  },
+                ),
+              ),
             ])));
   }
 }
