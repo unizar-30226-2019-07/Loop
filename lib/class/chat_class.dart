64c0864fdac41bb3f801cc092c044188bde5a318
vpec <vpe1@protonmail.com>
2019-05-02 14:15:04 +0200

    Iniciar chat en firebase

--- lib/class/chat_class.dart
+++ lib/class/chat_class.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:selit/class/item_class.dart';
 import 'package:selit/class/usuario_class.dart';
 import 'package:selit/util/api/api_config.dart';
 
@@ -6,8 +7,13 @@ import 'package:selit/util/api/api_config.dart';
 class ChatClass {
   
   UsuarioClass usuario;
+  int miId;
+  ItemClass producto;
+  bool visibleAnunciante;
+  bool visibleCliente;
   
 
-  ChatClass({this.usuario});
+  ChatClass({this.usuario, this.miId, this.producto, 
+    this.visibleAnunciante, this.visibleCliente});
   
 }
